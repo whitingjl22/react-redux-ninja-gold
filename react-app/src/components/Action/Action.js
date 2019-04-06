dcd2fa253c35cd653304418924e8f197a907e0b7
WHITING, JEREMY <jeremy.whiting23@gmail.com>
2019-04-06 18:15:19 -0500

    updates

--- react-app/src/components/Action/Action.js
+++ react-app/src/components/Action/Action.js
@@ -8,23 +8,42 @@ class Action extends React.Component {
   }
   render() {
     return (
-      <div className="flex-container">
-        <div>
-          <div className="action-container">
-            <h4>Component 1</h4>
-            <hr />
-            <li>Farm</li>
+      <div className="action-container">
+        <h4>Component 1</h4>
+        <hr />
+        <div className="flex-container">
+          <div className="farm">
+            <h3>Farm</h3>
             <p>Earns 2 - 5 Gold</p>
-            <button>Farm!</button>
-            <li>Cave</li>
+            <br />
+            <button>
+              <h4>Farm!</h4>
+            </button>
+          </div>
+          <div className="cave">
+            <h3>Cave</h3>
             <p>Earns 5 - 10 Gold</p>
-            <button>Cave!</button>
-            <li>Casino</li>
-            <p>Earns up to or Lose up to 100 Gold</p>
-            <button>Casino!</button>
-            <li>House</li>
+            <br />
+            <button>
+              <h4>Cave!</h4>
+            </button>
+          </div>
+          <div className="casino">
+            <h3>Casino</h3>
+            <p>
+              Earns up to or Lose up to 100 <br /> Gold
+            </p>
+            <button>
+              <h4>Casino!</h4>
+            </button>
+          </div>
+          <div className="house">
+            <h3>House</h3>
             <p>Earns 7 - 15 Gold</p>
-            <button>Hotel!</button>
+            <br />
+            <button>
+              <h4>Hotel!</h4>
+            </button>
           </div>
         </div>
       </div>
